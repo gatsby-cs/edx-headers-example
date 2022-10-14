8bca37c22cb1cab2e42df08ee5fa66a3fc12d4d4
Ben Robertson <hi@benrobertson.io>
2022-10-14 11:19:20 -0400

    disable default security headers.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,28 @@ module.exports = {
     {
       resolve: `gatsby-plugin-gatsby-cloud`,
       options: {
+        // option to add more headers
         headers: {
           "/event": [
-            "X-Frame-Options: ''" // I think this will override it
+            "X-XSS-Protection: 1; mode=block",
+            "X-Content-Type-Options: nosniff",
+            "Referrer-Policy: same-origin",
           ],
           "/images/*": [
             "Cache-Control: public, max-age=3600"
-          ]
-        }, // option to add more headers.
+          ],
+          "/*": [
+            "X-XSS-Protection: 1; mode=block",
+            "X-Content-Type-Options: nosniff",
+            "Referrer-Policy: same-origin",
+            "X-Frame-Options: DENY"
+          ],
+        },
+        mergeSecurityHeaders: false,
+        // option to add headers for all pages
         allPageHeaders: [
           "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
-        ], // option to add headers for all pages
+        ],
       },
     },
   ],
