d05fe3436caa9c5f69f6d42025e91952c5f469fa
Ben Robertson <hi@benrobertson.io>
2022-10-14 11:15:02 -0400

    Add gatsby plugin gatsby cloud config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {
+          "/event": [
+            "X-Frame-Options" // I think this will override it
+          ],
+          "/images/*": [
+            "Cache-Control: public, max-age=3600"
+          ]
+        }, // option to add more headers.
+        allPageHeaders: [
+          "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
+        ], // option to add headers for all pages
+      },
+    },
+  ],
 }
